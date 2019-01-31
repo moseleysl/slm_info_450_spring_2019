6fe551da2e4a2751d83140e9246a49a5c87cea92
Sequan Moseley <moseleysl@vcu.edu>
2019-01-30 19:54:30 -0500

    testing to link

---
+++ Week 2/practice1/practice1/practice1.cpp
@@ -0,0 +1,31 @@
+// practice1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+	int i = 0;
+	int response = 1;
+
+	cout << "Enter a number and I will tell you if it is prime or not. Enter 0 to close" << endl;
+
+	cin >> response;
+
+	while (response != 0)
+	{
+
+	}
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

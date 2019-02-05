0a25f3cf9b680ba1b787eff0e9ed4805eb10a5cf
moseleysl <46971739+moseleysl@users.noreply.github.com>
2019-02-05 13:15:29 -0500

    Update build_functions.cpp

--- Week3/build_functions.cpp
+++ Week3/build_functions.cpp
@@ -1,3 +1,34 @@
+#include <iostream>
+using namespace std;
+char  get_user_input();
+void to_upper_case(char &letter);
+int main(){
+  char user_input_letter;
+  cout << "Type in a letter grade, lowercase or uppercase." << endl; 
+  if ('0' != (user_input_letter = get_user_input())){
+    to_upper_case(user_input_letter);
+    switch (user_input_letter){
+      case 'A': cout << "Super Great job on an A!" << endl;break;
+      case 'B': cout << "Great job on a B!" << endl;break;
+      case 'C': cout << "Good job on a C!" << endl;break;
+      case 'D': cout << "Keep at it, you can get there." << endl;break;
+      case 'F': cout << "F is for #fail" << endl;break;
+      default : cout << "This is not a valid grade: " << user_input_letter << endl;
+    }
+  }
+  cout << "You have entered 0.  Exiting." << endl; 
+}
+char  get_user_input(){
+    
+    char input = 'a';
+    
+    cin >> &input;
+    
+   // cout << "You entered in " << input << endl;
+    
+    return input;
+   
+}
 void to_upper_case(char &letter){
     
     
@@ -20,5 +51,5 @@ void to_upper_case(char &letter){
         cout << "We have to go back... we've made a terrible mistake." << endl;
     }
     
-    cout << letter << " uppercased" << endl;
+   // cout << letter << " uppercased" << endl;
 }

47323347d82d2c26e0d0f585e2cd20ccc98a19f7
moseleysl <46971739+moseleysl@users.noreply.github.com>
2019-02-05 12:25:32 -0500

    Update circumference.cpp

--- Week3/circumference.cpp
+++ Week3/circumference.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+using namespace std;
 
+const double PI = 3.1415;
+
+double calculateCircumference(double radius);
+
+int main ()
+{
+   double input = 0;
+   double result = 0;
+   
+   while (1) {
+       cout << "Enter a radius of a circle and I will tell you the Circumference. " << endl;
+       cout << "Enter 0 to quit" << endl;
+       cin >> input;
+       
+       if (input == 0){
+           break;
+       }
+       
+       else{
+       result = calculateCircumference(input);
+       
+       cout << "A circle with " << input << " radius has a circumference of " << result << ". " << endl;
+       }
+   
+   }
+   
+   return 0;
+}
+
+double calculateCircumference(double radius){
+    
+    double circumference = 2 * PI * radius;
+    
+    return circumference;
+    
+}

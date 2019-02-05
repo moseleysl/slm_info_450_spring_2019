c313fdfc026e5b8ff1693b3d0a7b520054081218
moseleysl <46971739+moseleysl@users.noreply.github.com>
2019-02-05 13:15:07 -0500

    Update build_functions.cpp

--- Week3/build_functions.cpp
+++ Week3/build_functions.cpp
@@ -1,1 +1,24 @@
-
+void to_upper_case(char &letter){
+    
+    
+    if (letter == 'a'){ 
+        letter = 'A';
+    }
+    else if (letter == 'b'){ 
+        letter = 'B';
+    }
+    else if (letter == 'c'){ 
+        letter = 'C';
+    }
+    else if (letter == 'd'){ 
+        letter = 'D';
+    }
+    else if (letter == 'f'){ 
+        letter = 'F';
+    }
+    else{ 
+        cout << "We have to go back... we've made a terrible mistake." << endl;
+    }
+    
+    cout << letter << " uppercased" << endl;
+}
